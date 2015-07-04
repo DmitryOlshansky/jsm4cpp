6055fff8ce8633ba0cec69df1989cca30a3db70b
Dmitry Olshansky <dmitry.olsh@gmail.com>
2015-07-04 06:43:19 +0300

    And make revolver copy builds out of variant dirs

--- revolver.sh
+++ revolver.sh
@@ -9,11 +9,13 @@ read_batch(){
 }
 
 scons $@ 2>&1 | head -40
+cp build/*/gen-* .
 echo "[REVOLVER] Intial build is done."
 inotifywait -m -r src SConstruct -e modify | while true; do
 	BUF=$(read_batch)
 	if [ "x$BUF" != "x" ] ; then
 		scons $@ 2>&1 | head -40
+		cp build/*/gen-* .
 		echo "[REVOLVER] Done. Waiting for new events."
 	fi
 done
