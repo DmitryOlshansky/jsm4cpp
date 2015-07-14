25eb8be079093e72697016cdbe39fffe89985d7b
Dmitry Olshansky <dmitry.olsh@gmail.com>
2015-07-15 02:14:22 +0300

    tweak run all script

--- 0-run-all.sh
+++ 0-run-all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-(
-./1-*.sh
-./2-*.sh
-./3-*.sh
-./4-*.sh
-./5-*.sh
-./6-*.sh
-) > error-log.log
+runner(){
+	./1-*.sh
+	./2-*.sh
+	./3-*.sh
+	./4-*.sh
+	./5-*.sh
+	./6-*.sh
+}
+
+runner 2> error-log.log
